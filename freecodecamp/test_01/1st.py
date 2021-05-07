d920c2bd20063adf6e0c473e67e9a94249814020
Vinícius Cortez <vinisicius@gmail.com>
2021-05-07 17:38:27 -0300

    separando_features

--- freecodecamp/test_01/1st.py
+++ freecodecamp/test_01/1st.py
@@ -7,8 +7,8 @@ import tensorflow as tf
 import tensorflow.compat.v2.feature_column as fc
 
 
-dbtrain = pd.read_csv(r'C:\Users\vinis\Downloads\train.csv')
-dbtest = pd.read_csv(r'C:\Users\vinis\Downloads\eval.csv')
+x_train = pd.read_csv(r'C:\Users\vinis\Downloads\train.csv')
+x_test = pd.read_csv(r'C:\Users\vinis\Downloads\eval.csv')
 
 '''sns.pairplot(dbtrain)
 plt.show()
@@ -17,10 +17,22 @@ plt.show()
 
 so pega as colunas numericas......'''
 
-y_train = dbtrain.pop('survived')   #pop tira essa coluna do db e salva na variavel em questão
-y_test = dbtest.pop('survived')
+y_train = x_train.pop('survived')   #pop tira essa coluna do db e salva na variavel em questão
+y_test = x_test.pop('survived')
 
-pd.concat([dbtrain, y_train], axis=1).groupby('sex').survived.mean().plot(kind='barh').set_xlabel('% survived')
-plt.show()#porcentagem de sobreviventes por sexo
-print(dbtrain.info())
+pd.concat([x_train, y_train], axis=1).groupby('sex').survived.mean().plot(kind='barh').set_xlabel('% survived')
+#plt.show()#porcentagem de sobreviventes por sexo
+print(x_train.info())
 print(y_train)
+
+#Transformando categorias em numeros:
+feature_list = []
+categorical_column = ['sex', 'class', 'deck', 'embark_town', 'alone']
+numeric_column = ['age', 'fare', 'n_siblings_spouses', 'parch']
+for item in categorical_column:
+     vocabulary = x_train[item].unique()
+     feature_list.append(tf.feature_column.categorical_column_with_vocabulary_list(item, vocabulary))
+     #gera uma lista com os valores unicos de cada item e add eles
+for item in numeric_column:
+    feature_list.append(tf.feature_column.numeric_column(item, dtype=tf.float32))#add os valores numaricos
+print(feature_list)
